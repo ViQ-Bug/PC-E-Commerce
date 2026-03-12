d80b6fb25bba7c978f8fb50c4cddd4c3d8b34644
ViQ-Bug <ndc079.code.it@gmail.com>
2026-03-12 07:44:20 +0700

    order routes & controller edit

---
+++ backend/src/controllers/order.controller.js
@@ -0,0 +1,73 @@
+import { Order } from "../models/order.model.js";
+import { Product } from "../models/product.model.js";
+import { Review } from "../models/review.model.js";
+
+export async function createOrder(req, res) {
+  try {
+    const user = req.user;
+    const { orderItems, shippingAddress, paymentResult, totalPrice } = req.body;
+
+    if (!orderItems || orderItems.length === 0) {
+      return res
+        .status(400)
+        .json({ message: "No order items found in the request" });
+    }
+
+    for (const item of orderItems) {
+      const product = await Product.findById(item.product_id);
+      if (!product) {
+        return res
+          .status(404)
+          .json({ error: `Product ${item.name} not found` });
+      }
+      if (product.countInStock < item.quantity) {
+        return res
+          .status(400)
+          .json({ error: `Product ${product.name} is out of stock` });
+      }
+    }
+
+    const order = await Order.create({
+      user: user._id,
+      clerk: user.clerkId,
+      orderItems,
+      shippingAddress,
+      paymentResult,
+      totalPrice,
+    });
+
+    for (const item of orderItems) {
+      await Product.findByIdAndUpdate(item.product_id, {
+        $inc: { stock: -item.quantity },
+      });
+    }
+    res.status(201).json({ message: "Order created successfully", order });
+  } catch (error) {
+    console.error("Error creating order:", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+}
+export async function getUserOrder(req, res) {
+  try {
+    const orders = await Order.find({ clerkId: req.user.clerkId })
+      .populate("orderItems.product")
+      .sort({ createdAt: -1 });
+
+    const orderWithReviewStatus = await Promise.all(
+      orders.map(async (order) => {
+        const review = await Review.findOne({
+          orderId: order._id,
+        });
+        return {
+          ...order.toObject(),
+          hasReview: !!review,
+        };
+      }),
+    );
+
+    res.status(200).json({ orders: orderWithReviewStatus });
+  } catch (error) {
+    console.error("Error getting user order:", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+}
--- backend/src/server.js
+++ backend/src/server.js
@@ -10,6 +10,7 @@ import { connectDB } from "./config/db.js";
 
 import adminRoutes from "./routes/admin.route.js";
 import userRoutes from "./routes/user.route.js";
+import orderRoutes from "./routes/order.route.js";
 
 import dns from "dns";
 
@@ -26,6 +27,7 @@ app.use("/api/inngest", serve({ client: inngest, functions }));
 
 app.use("/api/admin", adminRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/order", orderRoutes);
 
 app.get("/api/health", (req, res) => {
   res.status(200).json({ message: "OK" });
