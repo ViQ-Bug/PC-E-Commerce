de7a32c04e34158af7d3488db937cecff332ba3a
ViQ-Bug <ndc079.code.it@gmail.com>
2026-04-01 08:18:04 +0700

    profile and wishlist added

---
+++ mobile/app/(profile)/privacy-security.tsx
@@ -0,0 +1,13 @@
+import SafeScreen from "@/components/SafeScreen";
+import React from "react";
+import { Text, View } from "react-native";
+
+function PrivacySecurityScreen() {
+  return (
+    <SafeScreen>
+      <Text className="text-white">PrivacySecurity</Text>
+    </SafeScreen>
+  );
+}
+
+export default PrivacySecurityScreen;
